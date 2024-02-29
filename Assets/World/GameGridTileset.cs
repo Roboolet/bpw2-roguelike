a3a6875a6ef4de2edb6db087035fb849cf90ee8d
Lea <39883637+Roboolet@users.noreply.github.com>
2024-02-29 06:48:00 +0100

    extra map and chest tiles

--- Assets/World/GameGridTileset.cs
+++ Assets/World/GameGridTileset.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class GameGridTileset : ScriptableObject
 {
     const int textureSeed = 2003;
-    public Sprite[] t_wall, t_ladder, t_stairRight, t_stairLeft, t_light;
+    public Sprite[] t_wall, t_ladder, t_stairRight, t_stairLeft, t_light, t_chest;
 
     public Sprite GetSpriteByTileType(GridTileType type, int x, int y)
     {
@@ -25,6 +25,8 @@ public class GameGridTileset : ScriptableObject
             case GridTileType.StairLeft: return t_stairLeft[Mathf.RoundToInt(seedVal * (t_stairLeft.Length - 1))];
 
             case GridTileType.LightSource: return t_light[Mathf.RoundToInt(seedVal * (t_light.Length - 1))];
+
+            case GridTileType.Chest: return t_chest[Mathf.RoundToInt(seedVal * (t_chest.Length - 1))];
 
             default: return null;
         }
